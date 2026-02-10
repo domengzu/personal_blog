c88565576ce5ed95da15015bedfe9be99b89132b
dom.dom <dyasuham@gmail.com>
2026-02-10 21:26:19 +0800

    feat: implement reactions feature with UI updates and controller enhancements

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,19 +1,18 @@
 class ReactionsController < ApplicationController
   before_action :authenticate_user!
   def create
-    post = Post.find(params[:post_id])
-    post.reactions.create(user: current_user)
+    @post = Post.find(params[:post_id])
+    @post.reactions.find_or_create_by(user: current_user)
+
+    redirect_to @post
   end
 
   def destroy
-    reaction = Reaction.find(params[:id])
-    reaction.destroy if reaction.user == current_user
-    redirect_to reaction.post
+    @reaction = Reaction.find(params[:id])
+    @post = @reaction.post
+
+    @reaction.destroy if @reaction.user == current_user
+
+    redirect_to @post
   end
-
-  # private
-
-  # def reaction_params
-  #   params.require(:reaction).permit(:kind)
-  # end
 end
